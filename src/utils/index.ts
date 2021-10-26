ccee3b9650f1ef6879a30f81a04b489d8fe5b19b
whh01016728 <whh01016728@alibaba-inc.com>
2021-10-26 16:05:51 +0800

    feat: 完善list-node

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,63 @@
 export class ListNode {
   val: number
   next: ListNode | null
-  constructor(val: number, next: ListNode | null) {
+  private currentNode: ListNode | null | undefined
+  constructor(val?: number, next?: ListNode | null) {
     this.val = (val === undefined ? 0 : val)
     this.next = (next === undefined ? null : next)
+    return this
+  }
+  getNode(val: number) {
+    let currentNode = null
+    if(this.val === val) {
+      currentNode = this
+    }
+    this.currentNode = this.next
+    while(this.currentNode) {
+      if(this.currentNode.val === val) {
+        currentNode = this.currentNode
+      }
+      this.currentNode = this.currentNode.next
+    }
+    return currentNode
+  }
+  addNode(val: number) {
+    if(!this.next) {
+      this.next = new ListNode(val, null)
+      return this
+    }
+    this.currentNode = this.next
+    let currentNode = null
+    while(this.currentNode) {
+      if(!this.currentNode.next) {
+        currentNode = this.getNode(this.currentNode.val)
+      }
+      this.currentNode = this.currentNode?.next
+    }
+    if(currentNode) {
+      currentNode.next = new ListNode(val, null);
+    }
+    return this
+  }
+  editNode(oldVal: number, newVal: number) {
+    if(oldVal === this.val) {
+      this.val = newVal
+      return this
+    }
+    let currentNode = this.getNode(oldVal)
+    if(currentNode?.val) {
+      currentNode.val = newVal;
+    }
+    return this
+  }
+  deleteNode(val: number) {
+    if(val === this.val) {
+      this.next = null
+    }
+    let currentNode = this.getNode(val);
+    if(currentNode?.val) {
+      currentNode.next = null
+    }
+    return this
   }
 }
--- test/utils/list-node.test.ts
+++ test/utils/list-node.test.ts
@@ -1,8 +1,8 @@
 import { ListNode } from '../../src/utils/index'
 
 describe('listNode tobe 2, 4, 3', () => {
-
   const result = new ListNode(2, new ListNode(4, new ListNode(3, null)))
+  const testList = new ListNode()
 
   test('tobe 2', async () => {
     expect(result.val).toBe(2)
@@ -12,7 +12,21 @@ describe('listNode tobe 2, 4, 3', () => {
   })
 
   test('tobe 3', async () => {
-    expect(result.next?.next?.val).toBe(3)
+    expect(result.getNode(3)?.val).toBe(3)
+  })
+  test('getNode tobe 4', async () => {
+    expect(result.getNode(4)?.val).toBe(4)
+  })
+  test('add node', async () => {
+    expect(testList.addNode(2).next?.val).toBe(2)
+  })
+  test('edit node', async () => {
+    expect(testList.editNode(2, 3).next?.val).toBe(3)
+  })
+  test('delete node', async () => {
+    testList.addNode(5);
+    expect(testList.getNode(5)?.val).toBe(5)
+    expect(testList.deleteNode(3).next?.next).toBe(null)
   })
 })
 
